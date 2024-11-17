a4ac3deff90f5cefad784bf8ad90af5fb9890179
serenafabbiann <fabbiansere@gamil.com>
2024-11-17 19:50:05 -0300

    home instalacion de tab navigation

--- my-rn-project/App.js
+++ my-rn-project/App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { creeateBottomTabNavigator } from '@react-navigaton/bottom-tabs';
+
+const Tab = creeateBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={ Home } />
+        <Tab.Screen name="Profile" component={ Profile } />
+        <Tab.Screen name="NewPost" component={ NewPost } />
+        <Tab.Screen name="Users" component={ Users } />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ my-rn-project/src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { creeateBottomTabNavigator } from '@react-navigaton/bottom-tabs';
+//importar Home de screen
+//importar NewPost de screen
+//importar Profile de screen
+//importar Users de screen
+
+const Tab = creeateBottomTabNavigator();
+
+export default class HomePage extends Component {
+    render(){
+        return(
+            
+        )
+    }
+}

8742f0a51ffbbd837f2e5c5288b77309e92b9d8e
Josefina Palmero <josepalmero@MacBook-Air-de-Josefina-4.local>
2024-11-19 17:28:24 -0300

    menu de navegacion

--- my-rn-project/App.js
+++ my-rn-project/App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { creeateBottomTabNavigator } from '@react-navigaton/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { creeateBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Login from './src/screens/Login'; //agregar login --> stack navigation dentro de HomeMenu creoooo!!
 import Register from './src/screens/Register'; //agregar register --> stack navigation dentro de HomeMenu creoooo!! 
 import Home from './src/screens/Home';
@@ -9,16 +10,22 @@ import Profile from './src/screens/Profile';
 import NewPost from './src/screens/NewPost';
 import Users from './src/screens/Users';
 
-const Tab = creeateBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component = { Login } />
+        <Stack.Screen name="Register" component = { Register } />
+      </Stack.Navigator>
+
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={ Home } />
-        <Tab.Screen name="Profile" component={ Profile } />
-        <Tab.Screen name="NewPost" component={ NewPost } />
-        <Tab.Screen name="Users" component={ Users } />
+        <Tab.Screen name="Home" component = { Home } />
+        <Tab.Screen name="Profile" component = { Profile } />
+        <Tab.Screen name="NewPost" component = { NewPost } />
+        <Tab.Screen name="Users" component = { Users } />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- my-rn-project/src/components/HomePage.js
+++ my-rn-project/src/components/HomePage.js
@@ -1,16 +1,53 @@
 import React, { Component } from "react";
-import { creeateBottomTabNavigator } from '@react-navigaton/bottom-tabs';
-//importar Home de screen
-//importar NewPost de screen
-//importar Profile de screen
-//importar Users de screen
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home';
+import Profile from '../screens/Profile';
+import NewPost from '../screens/NewPost';
+import Users from '../screens/Users'
+import { auth } from '../firebase/config'
+import { Entypo, FontAwesome, FontAwesome6 } from '@expo/vector-icons';
 
-const Tab = creeateBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 export default class HomePage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    componentDidMount(){
+        auth.onAuthStateChanged((user) => { 
+            if (!user) {
+                this.props.navigation.navigate("Login")
+            }
+        })
+    }
+
     render(){
         return(
-            
+            <Tab.Navigator screenOptions={{tabBarShowLabel: false, headerShown: false }}>
+                <Tab.Screen 
+                    name = "Home"
+                    component = {Home}
+                    options = { {tabBarIcon: () => <Entypo name="home" size={24} color="black" />} }
+                />
+                <Tab.Screen 
+                    name = "Profile"
+                    component = {Profile}
+                    options = { {tabBarIcon: () => <FontAwesome name="user" size={24} color="black" />} }
+                />
+                <Tab.Screen 
+                    name = "NewPost"
+                    component = {NewPost}
+                    options = { {tabBarIcon: () => <FontAwesome6 name="newPost" size={24} color="black" />} }
+                />
+                <Tab.Screen 
+                    name = "Users"
+                    component = {Users}
+                    options = { {tabBarIcon: () => <FontAwesome name="search" size={24} color="black" />} }
+                />
+            </Tab.Navigator>
         )
     }
 }

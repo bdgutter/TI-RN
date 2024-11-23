233b623a7618b98683c253ba13422bd596ae2c9b
serenafabbiann <fabbiansere@gamil.com>
2024-11-23 17:09:20 -0300

    likear posteos wip

--- my-rn-project/src/components/Post.js
+++ my-rn-project/src/components/Post.js
@@ -7,29 +7,64 @@ export default class Post extends Component{
     constructor(props){
         super(props)
         this.state = {
-            posts: this.props.posts
+            posts: this.props.posts,
+            like: false,
+            cantidadLikes: this.props.item.data.likes.length
+        }
+    }
+
+    componentDidMount(){
+        auth.onAuthStateChanged((user) => { 
+            if (!user) {
+                this.props.navigation.navigate("Login")
+            }
+        })
+        if(this.props.item.data.likes.includes(auth.currentUser.email)){
+            this.setState({
+                like: true
+            })
         }
     }
 
 
 
 handelLike(){
-
+    db.collection("posts").doc(this.props.item.id).update({
+        likes: firebase.firestore.FieldValue.arrayUnion(this.props.item.data.owner)
+    })
+    .then(() => this.setState({
+        like: true,
+        cantidadLikes: this.props.item.data.likes.length
+    }))
 }
 handelNotLike(){
-
+    db.collection("posts").doc(this.props.item.id).update({
+        likes: firebase.firestore.FieldValue.arrayUnion(this.props.item.data.owner)
+    })
+    .then(() => this.setState({
+        like: false,
+        cantidadLikes: this.props.item.data.likes.length
+    }))
 }
 
     render(){
         const {posts} = this.state
-        ////en el pate de likes falta el touchableOpacity donde podes likear y deslikear
-        ////y la cantidad de likes
+
         return(
             <View>
                 <Text>Posteos</Text>
                 <Text>Usuario: {posts.data.owner}</Text>
                 <Text>Descripcion: {posts.data.text}</Text>
-                <Text>Likes: {posts.data.likes}</Text> 
+                {this.state.like ? (
+                    <TouchableOpacity onPress={() => this.handelLike()}>
+                        <Text>Me gusta</Text>
+                    </TouchableOpacity>
+                ) : (
+                    <TouchableOpacity onPress={() => this.handelNotLike()}>
+                        <Text>No me gusta mas</Text>
+                    </TouchableOpacity>
+                )}
+                <Text>Cantidad de Likes: {this.state.cantidadLikes}</Text>
                 <Text>Fecha de Publicacion: </Text>
             </View>
         )

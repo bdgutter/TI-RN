6dcfee57ef1511938d3f2af7b8d7c071be7d0a9d
Josefina Palmero <josepalmero@MacBook-Air-de-Josefina-4.local>
2024-11-23 20:23:25 -0300

    menu de navegacion completo

--- my-rn-project/App.js
+++ my-rn-project/App.js
@@ -1,41 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Login from './src/screens/Login'; 
 import Register from './src/screens/Register'; 
-import Profile from './src/screens/Profile';
-import NewPost from './src/screens/NewPost';
-import Users from './src/screens/Users';
-import Home from './src/screens/Home';
+import HomePage from './src/components/HomePage'
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+      <Stack.Screen name="HomePage" component = { HomePage } options = {{headerShown: false}} />
         <Stack.Screen name="Login" component = { Login } />
         <Stack.Screen name="Register" component = { Register } />
       </Stack.Navigator>
-
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component = { Home } />
-        <Tab.Screen name="Profile" component = { Profile } />
-        <Tab.Screen name="NewPost" component = { NewPost } />
-        <Tab.Screen name="Users" component = { Users } />
-      </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- my-rn-project/src/components/HomePage.js
+++ my-rn-project/src/components/HomePage.js
@@ -13,7 +13,6 @@ export default class HomePage extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {}
     }
 
     componentDidMount(){

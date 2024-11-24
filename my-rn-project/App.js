e14cccff995b1026f7087a456ba06d8cf030d18a
Josefina Palmero <josepalmero@MacBook-Air-de-Josefina-4.local>
2024-11-24 14:31:58 -0300

    formato

--- my-rn-project/App.js
+++ my-rn-project/App.js
@@ -4,14 +4,13 @@ import Login from './src/screens/Login';
 import Register from './src/screens/Register'; 
 import HomePage from './src/components/HomePage'
 
-
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen name="HomePage" component = { HomePage } options = {{headerShown: false}} />
+        <Stack.Screen name="HomePage" component = { HomePage } options = {{headerShown: false}} />
         <Stack.Screen name="Login" component = { Login } />
         <Stack.Screen name="Register" component = { Register } />
       </Stack.Navigator>
